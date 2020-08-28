cc6bb2a5707a53edf5000ea83de4ff0e263465db
Johan Lagerqvist <johan@tryhard.me>
2020-08-28 13:17:59 +0900

    Updates

---
+++ precache-manifest.9fa5aa50422144576ee1c93bc24ee1c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c3b8dad39c653a89c67060120e2a11b",
+    "url": "/hoi-fragger/index.html"
+  },
+  {
+    "revision": "e162ec6d8bd6870a8721",
+    "url": "/hoi-fragger/static/js/2.3053baf4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hoi-fragger/static/js/2.3053baf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2798683053d7a27844b1",
+    "url": "/hoi-fragger/static/js/main.5084ce3b.chunk.js"
+  },
+  {
+    "revision": "35fcf5ef0c49ac4d1157",
+    "url": "/hoi-fragger/static/js/runtime-main.ab371fa9.js"
+  },
+  {
+    "revision": "8c680e74feeb564e962005221eb2e14d",
+    "url": "/hoi-fragger/static/media/bulge.8c680e74.svg"
+  }
+]);
